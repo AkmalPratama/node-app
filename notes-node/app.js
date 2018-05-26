e0a81b964901f0c6add9e454afc9bf048c25b52d
Akmal <m.akmalpratama@yahoo.co.id>
2018-05-26 17:05:06 +0700

    section 10 requiring own files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,11 @@
 console.log('Starting app.');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFileSync('greetings.txt', 'Hello world!');
+console.log('Result:', notes.add(12, 8));
+
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
