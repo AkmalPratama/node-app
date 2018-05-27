5239376fc7ada0f0bca60157d7e4692d570e5de7
Akmal <m.akmalpratama@yahoo.co.id>
2018-05-27 14:36:52 +0700

    sect 13 user input

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,22 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Akmal'));
-var filteredArray = _.uniq(['Yololo', 1, 'Akmal', 1, 2 , 3, 4]);
-console.log(filteredArray);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-// console.log('Result:', notes.add(12, 8));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if (command == 'add') {
+    console.log('Adding new note');
+} else if (command == 'list') {
+    console.log('Listing all notes');
+} else if (command == 'read') {
+    console.log('Reading note');
+} else if (command == 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recognized');
+}
