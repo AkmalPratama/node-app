0a02509b0cfa5a21676abde586387db638d1a610
Akmal <m.akmalpratama@yahoo.co.id>
2018-05-28 23:57:33 +0700

    sect 18 remove note

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -41,7 +41,9 @@ var getNote = (title) => {
 };
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
 };
 
 module.exports = {
